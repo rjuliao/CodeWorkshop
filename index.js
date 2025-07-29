d10eecd35dc135098edbbbed6c465e4681b8b52b
Raul Juliao <raulj@uninorte.edu.co>
2025-07-29 17:24:51 -0500

    feat: Initialize Express server and configure doctors router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 const doctorsRouter = require("./doctors/doctors");
 
 app.use("/doctors", doctorsRouter);
